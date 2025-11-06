0b8d571b49e99bb6f2cf560d1b34195e386a5e76
Ayoub <ayoubjarhni@gmail.com>
2025-11-06 21:20:00 +0100

    setup initial

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "<h1 style='text-align:center; color:#0078D4; margin-top:50px;'>Hello from Azure Web App 👋</h1>";
+echo "<p style='text-align:center;'>Votre application PHP est déployée avec succès sur Azure !</p>";
+
+echo "<hr>";
+echo "<p style='text-align:center; color:gray;'>PHP version : " . phpversion() . "</p>";
+?>
