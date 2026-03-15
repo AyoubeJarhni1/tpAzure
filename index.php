4842b248f0c9e490bb7133086b209dcc3b18ae04
AyoubeJarhni1 <ayoubjarhni@gmail.com>
2026-03-15 23:51:02 +0000

    index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<h1 style='text-align:center; color:#0078D4; margin-top:50px;'>Hello from Azure Web Application 👋</h1>";
+echo "<h1 style='text-align:center; color:#0078D4; margin-top:50px;'>Hello from Azure Web Application test 👋</h1>";
 echo "<p style='text-align:center;'>Votre application hhdhdhdhdhdhd est déployée avec succès sur Azure !</p>";
 
 echo "<hr>";
